cdba49dbc3e95ba302b8c23839e925fbb75ae3e9
Tyler Doster <tjdfalcon@gmail.com>
2018-03-31 23:03:43 -0700

    move preference management outside of service

--- app/src/main/java/dapathy/com/rotationmanager/ReceiverManagerService.java
+++ app/src/main/java/dapathy/com/rotationmanager/ReceiverManagerService.java
@@ -3,9 +3,7 @@ package dapathy.com.rotationmanager;
 import android.app.Service;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.SharedPreferences;
 import android.os.IBinder;
-import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
@@ -41,7 +39,6 @@ public class ReceiverManagerService extends Service {
 	private void registerReceiver() {
 		if (!Utility.hasSettingPermissions(this)) return;
 
-		setEnabled(true);
 		IntentFilter receiverFilter = new IntentFilter(Intent.ACTION_HEADSET_PLUG);
 
 		if (receiver == null)
@@ -50,17 +47,9 @@ public class ReceiverManagerService extends Service {
 	}
 
 	private void unregisterReceiver() {
-		setEnabled(false);
 		if (receiver == null) return;
 
 		receiver.revert(this);
 		this.unregisterReceiver(receiver);
-	}
-
-	private void setEnabled(boolean value) {
-		SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-		SharedPreferences.Editor editor = sharedPreferences.edit();
-		editor.putBoolean(Utility.ENABLED, value);
-		editor.apply();
 	}
 }
